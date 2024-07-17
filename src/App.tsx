f2f113072662c542f5fb0695cc718bda9f40eb63
Henning Cullin <henningcullin@gmail.com>
2024-07-17 18:24:34 +0200

    refactor: remove deps and include directoryform

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import { Button } from "./components/ui/button";
-
 import "./App.css";
 import "../app/globals.css";
 import { Films } from "./films/Films";
+import { DirectoryForm } from "./directories/DirectoryForm";
 
 function App() {
   return (
     <div>
+      <DirectoryForm></DirectoryForm>
       <Films></Films>
     </div>
   );
