0d83744ae5dcfbe3edcf88e87e9015ea5da5654d
Henning Cullin <henningcullin@gmail.com>
2024-07-17 22:59:26 +0200

    refactor: move submitter inside component

--- src/directories/DirectoryForm.tsx
+++ src/directories/DirectoryForm.tsx
@@ -8,32 +8,30 @@ import { Label } from "@/components/ui/label";
 export function DirectoryForm() {
   const [directory, setDirectory] = useState<string>("");
 
+  async function addDirectory(path: string) {
+    try {
+      await invoke("add_directory", { path });
+      setDirectory("");
+    } catch (error) {}
+  }
+
   async function selectDirectory() {
     try {
       const dir: string | null = await invoke("select_directory");
-      if (typeof dir === "string") setDirectory(dir);
+      if (typeof dir === "string" && dir) setDirectory(dir);
     } catch (error) {
       console.error("Failed to select directory", error);
     }
   }
 
   return (
-    <>
-      <div style={{ width: "450px" }}>
-        <Label>Directory</Label>
-        <div style={{ display: "flex", gap: "5px" }}>
-          <Input value={directory} readOnly disabled />
-          <Button onClick={selectDirectory}>Select Directory</Button>
-        </div>
+    <form style={{ width: "450px" }}>
+      <Label>Directory</Label>
+      <div style={{ display: "flex", gap: "5px" }}>
+        <Input value={directory} readOnly disabled />
+        <Button onClick={selectDirectory}>Select Directory</Button>
       </div>
-    </>
+      <Button onClick={() => addDirectory(directory)}>Add Directory</Button>
+    </form>
   );
-}
-
-async function addDirectory(path: string) {
-  try {
-    await invoke("add_directory", { path });
-  } catch (error) {
-    console.error("Failed to add directory:", error);
-  }
 }
