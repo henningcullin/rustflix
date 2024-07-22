82154dd842f5b706a26874c990d2d3968350400c
Henning Cullin <henningcullin@gmail.com>
2024-07-22 09:15:06 +0200

    feat: table of all file entries

--- src/films/Films.tsx
+++ src/films/Films.tsx
@@ -1,3 +1,20 @@
+import { invoke } from '@tauri-apps/api/tauri';
+import { useEffect, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { CheckIcon, Cross2Icon } from '@radix-ui/react-icons';
+interface Entry {
+  directory: string;
+  path: string;
+  film?: Film;
+}
+
 export interface Film {
   id: number;
   file: string;
@@ -9,10 +26,83 @@ export interface Film {
 }
 
 function Films() {
+  const [entries, setEntries] = useState<Entry[]>([]);
+
+  async function initialize() {
+    try {
+      const fileMap = JSON.parse(await invoke('get_all_files'));
+      const files: Entry[] = [];
+
+      // Flatten the fileMap to an array of { directory, path } objects
+      for (const [directory, filmArray] of Object.entries(fileMap)) {
+        if (!Array.isArray(filmArray)) continue;
+        for (const film of filmArray) {
+          files.push({
+            directory,
+            path: film,
+          });
+        }
+      }
+
+      const films: Film[] | null = await invoke('get_all_films');
+
+      if (!films?.length) return setEntries(files);
+
+      // Create a map for quick lookup of films by their file property
+      const filmMap = new Map<string, Film>();
+      for (const film of films) {
+        filmMap.set(film.file, film);
+      }
+
+      // Combine files and films based on matching path and file properties
+      const combinedEntries = files.map((file) => ({
+        ...file,
+        film: filmMap.get(file.path),
+      }));
+
+      setEntries(combinedEntries);
+    } catch (error) {
+      console.error('Failed to fetch films:', error);
+    }
+  }
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Directory</TableHead>
+          <TableHead>Path</TableHead>
+          <TableHead>Is Registered</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {entries.map((entry) => (
+          <TableRow>
+            <TableCell>{entry.directory}</TableCell>
+            <TableCell>{entry.path}</TableCell>
+            <TableCell>
+              <CheckBox state={entry?.film?.id} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+function CheckBox({ state }: { state: number | undefined | null }) {
+  return (
+    <>
+      {state !== null && state !== undefined ? (
+        <CheckIcon className='text-green-700 ' />
+      ) : (
+        <Cross2Icon className='text-red-700 ' />
+      )}
+    </>
   );
 }
 
