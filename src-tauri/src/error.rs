cc53fddc070f19d1327c7d043899a1e0519ab4b6
Henning Cullin <henningcullin@gmail.com>
2024-07-29 15:12:45 +0200

    feat: more error types included

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,22 +1,41 @@
+use reqwest::Error as reqwestError;
 use rusqlite::Error as rusqliteError;
+use scraper::error::SelectorErrorKind as SelectorError;
 use serde_json::Error as serdeError;
+
+use std::error::Error;
 use std::fmt;
 
 #[derive(Debug)]
 pub enum AppError {
-    DatabaseError(rusqliteError),
+    DatabaseError(rusqlite::Error),
     JsonError(serdeError),
+    SelectorError(SelectorError),
+    ReqwestError(reqwestError),
+    ScrapeError(String),
 }
 
 impl From<rusqliteError> for AppError {
-    fn from(error: rusqliteError) -> Self {
-        Self::DatabaseError(error)
+    fn from(value: rusqliteError) -> Self {
+        Self::DatabaseError(value)
     }
 }
 
 impl From<serdeError> for AppError {
-    fn from(error: serdeError) -> Self {
-        Self::JsonError(error)
+    fn from(value: serdeError) -> Self {
+        Self::JsonError(value)
+    }
+}
+
+impl From<SelectorError> for AppError {
+    fn from(value: SelectorError) -> Self {
+        Self::SelectorError(value)
+    }
+}
+
+impl From<reqwestError> for AppError {
+    fn from(value: reqwestError) -> Self {
+        Self::ReqwestError(value)
     }
 }
 
@@ -25,15 +44,19 @@ impl fmt::Display for AppError {
         match *self {
             AppError::DatabaseError(ref err) => write!(f, "Database error: {}", err),
             AppError::JsonError(ref err) => write!(f, "JSON error: {}", err),
+            AppError::SelectorError(ref err) => write!(f, "Selector error: {}", err),
+            AppError::ScrapeError(ref err) => write!(f, "Scrape error: {}", err),
         }
     }
 }
 
 impl std::error::Error for AppError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
         match *self {
             AppError::DatabaseError(ref err) => Some(err),
             AppError::JsonError(ref err) => Some(err),
+            AppError::SelectorError(ref err) => Some(err),
+            AppError::ScrapeError(_) => None,
         }
     }
 }
