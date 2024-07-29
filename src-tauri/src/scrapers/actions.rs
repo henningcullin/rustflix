73387c0d676ee46ac2ad81253722badeb9b9f518
Henning Cullin <henningcullin@gmail.com>
2024-07-29 15:13:00 +0200

    refactor: more graceful error handling

--- src-tauri/src/scrapers/actions.rs
+++ src-tauri/src/scrapers/actions.rs
@@ -1,18 +1,19 @@
 use scraper::{Html, Selector};
 use serde::Deserialize;
 
+use crate::error::AppError;
 #[derive(Debug, Deserialize)]
 struct Film {
-    title: String,
+    title: Option<String>,
     genres: Vec<String>,
-    release_date: String,
-    plot: String,
-    run_time: String,
-    color: String,
+    release_date: Option<String>,
+    plot: Option<String>,
+    run_time: Option<String>,
+    color: Option<String>,
     directors: Vec<Director>,
     stars: Vec<Star>,
-    cover: String,
-    rating: f64,
+    cover: Option<String>,
+    rating: Option<f64>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -29,50 +30,56 @@ struct Star {
     avatar: Option<String>,
 }
 
-pub async fn scrape_movie(id: &str) -> Result<Film, Box<dyn std::error::Error>> {
+pub async fn scrape_movie(id: &str) -> Result<Film, AppError> {
     let url = format!("https://www.imdb.com/title/{}/", id);
     let raw_html = get_page(&url).await?;
     let parsed_html = Html::parse_document(&raw_html);
 
     let data_object: serde_json::Value = get_data_object(&parsed_html)?;
+
     let info = Film {
-        title: unescape(data_object["name"].as_str().unwrap_or_default()),
+        title: Some(unescape(data_object["name"].as_str().unwrap_or_default())),
         genres: data_object["genre"]
             .as_array()
             .unwrap_or(&vec![])
             .iter()
             .map(|g| unescape(g.as_str().unwrap_or_default()))
             .collect(),
-        release_date: data_object["datePublished"].to_string(),
-        plot: unescape(data_object["description"].as_str().unwrap_or_default()),
-        run_time: data_object["duration"]
-            .as_str()
-            .unwrap_or_default()
-            .to_string(),
-        color: color(&parsed_html).unwrap_or_default(),
+        release_date: Some(data_object["datePublished"].to_string()),
+        plot: Some(unescape(
+            data_object["description"].as_str().unwrap_or_default(),
+        )),
+        run_time: Some(
+            data_object["duration"]
+                .as_str()
+                .unwrap_or_default()
+                .to_string(),
+        ),
+        color: color(&parsed_html).ok(),
         directors: directors(&data_object),
         stars: stars(&parsed_html),
-        cover: data_object["image"].to_string(),
+        cover: Some(data_object["image"].to_string()),
         rating: data_object["aggregateRating"]["ratingValue"]
             .as_f64()
-            .unwrap_or_default(),
+            .unwrap_or_default()
+            .into(),
     };
 
     Ok(info)
 }
 
-async fn get_page(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+async fn get_page(url: &str) -> Result<String, AppError> {
     let response = reqwest::get(url).await?;
     let html = response.text().await?;
     Ok(html)
 }
 
-fn get_data_object(parsed_html: &Html) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-    let selector = Selector::parse(r#"[type="application/ld+json"]"#).unwrap();
+fn get_data_object(parsed_html: &Html) -> Result<serde_json::Value, AppError> {
+    let selector = Selector::parse(r#"[type="application/ld+json"]"#)?;
     let script_element = parsed_html
         .select(&selector)
         .next()
-        .ok_or("No script element found")?;
+        .ok_or(AppError::ScrapeError("No script element found".to_string()))?;
     let script_text = script_element.inner_html();
     let data_object: serde_json::Value = serde_json::from_str(&script_text)?;
     Ok(data_object)
@@ -82,18 +89,23 @@ fn unescape(string: &str) -> String {
     html_escape::decode_html_entities(string).to_string()
 }
 
-fn color(parsed_html: &Html) -> Result<String, Box<dyn std::error::Error>> {
-    let selector = Selector::parse(r#"[data-testid="title-techspec_color"]"#)?;
+fn color(parsed_html: &Html) -> Result<String, AppError> {
+    let selector = Selector::parse(r#"[data-testid="title-techspec_color"]"#)
+        .map_err(|e| AppError::ScrapeError(e.to_string()))?;
     let color_element = parsed_html.select(&selector).next();
+
     match color_element {
-        Some(element) => element
-            .last_child()
-            .unwrap(),
-            .value()
-            .as_text()
-            .unwrap()
-            .to_string(),
-        None => Err("No text"),
+        Some(element) => {
+            if let Some(last_child) = element.last_child() {
+                if let Some(text) = last_child.value().as_text() {
+                    return Ok(text.to_string());
+                }
+            }
+            Err(AppError::ScrapeError("Color text not found".to_string()))
+        }
+        None => Err(AppError::ScrapeError(
+            "Color information not found".to_string(),
+        )),
     }
 }
 
@@ -138,7 +150,8 @@ fn stars(parsed_html: &Html) -> Vec<Star> {
             let character = element
                 .select(&character_selector)
                 .next()
-                .map(|e| unescape(e.first_child().unwrap().value().as_text().unwrap()))
+                .and_then(|e| e.first_child().and_then(|c| c.value().as_text()))
+                .map(|item| unescape(item))
                 .unwrap_or_default();
 
             let actor_element = element.select(&actor_selector).next().unwrap();
@@ -146,10 +159,10 @@ fn stars(parsed_html: &Html) -> Vec<Star> {
             let imdb_id = actor_element
                 .value()
                 .attr("href")
-                .unwrap()
+                .unwrap_or_default()
                 .split("/")
                 .nth(4)
-                .unwrap()
+                .unwrap_or_default()
                 .to_string();
 
             Star {
