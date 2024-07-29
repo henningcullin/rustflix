fb7d04851331dc16893198cd37617b76dabaadf8
Henning Cullin <henningcullin@gmail.com>
2024-07-29 14:56:50 +0200

    feat: impl Error trait and Display

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,5 +1,6 @@
 use rusqlite::Error as rusqliteError;
 use serde_json::Error as serdeError;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum AppError {
@@ -16,5 +17,23 @@ impl From<rusqliteError> for AppError {
 impl From<serdeError> for AppError {
     fn from(error: serdeError) -> Self {
         Self::JsonError(error)
+    }
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            AppError::DatabaseError(ref err) => write!(f, "Database error: {}", err),
+            AppError::JsonError(ref err) => write!(f, "JSON error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for AppError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            AppError::DatabaseError(ref err) => Some(err),
+            AppError::JsonError(ref err) => Some(err),
+        }
     }
 }
