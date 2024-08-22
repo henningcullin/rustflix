d8451e13e282e88c01e8143c2910d5ea319918b1
Henning Cullin <henningcullin@gmail.com>
2024-08-22 21:12:19 +0200

    fix: dont execute in batch and has_watch not null default 0

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -22,25 +22,42 @@ pub fn create_connection() -> Result<Connection, rusqlite::Error> {
 pub fn initialize_database() -> Result<(), rusqlite::Error> {
     let conn = create_connection()?;
 
-    conn.execute_batch(
+    conn.execute(
         r#"--sql
         PRAGMA foreign_keys = ON
+        "#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS directories (
             id   INTEGER PRIMARY KEY,
             path TEXT NOT NULL
-        )
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS genres (
             id  INTEGER PRIMARY KEY,
             path TEXT NOT NULL
-        )
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS languages (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL
-        )
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS persons (
             id INTEGER PRIMARY KEY,
             imdb_id TEXT,
@@ -48,15 +65,23 @@ pub fn initialize_database() -> Result<(), rusqlite::Error> {
             age INTEGER,
             gender INTEGER,
             birthplace INTEGER
-        )
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS characters (
             id INTEGER PRIMARY KEY,
             actor INTEGER NOT NULL,
             description TEXT NOT NULL,
-            FOREIGN KEY (actor) REFERENCES persons(id) ON DELETE 
-        )
+            FOREIGN KEY (actor) REFERENCES persons(id) ON DELETE CASCADE
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS films (
             id              INTEGER PRIMARY KEY,
             file            TEXT NOT NULL,
@@ -69,51 +94,71 @@ pub fn initialize_database() -> Result<(), rusqlite::Error> {
             color           INTEGER,
             rating          INTEGER,
             cover_image     TEXT,
-            has_watched     INTEGER,
+            has_watched     INTEGER NOT NULL DEFAULT 0,
             left_off_point  INTEGER,
-            registered  INTEGER NOT NULL DEFAULT 0,
+            registered      INTEGER NOT NULL DEFAULT 0,
             FOREIGN KEY (directory) REFERENCES directories(id) ON DELETE CASCADE
-        )
-        
+        )"#,
+        [],
+    )?;
+
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS film_genres (
             film_id INTEGER NOT NULL,
             genre_id INTEGER NOT NULL,
             FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
             FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE,
             PRIMARY KEY (film_id, genre_id)
-        );
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS film_languages (
             film_id INTEGER NOT NULL,
             language_id INTEGER NOT NULL,
             FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
             FOREIGN KEY (language_id) REFERENCES languages(id) ON DELETE CASCADE,
             PRIMARY KEY (film_id, language_id)
-        );
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS film_keywords (
             film_id INTEGER NOT NULL,
             keyword TEXT NOT NULL,
             FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
             PRIMARY KEY (film_id, keyword)
-        );
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS film_directors (
             film_id INTEGER NOT NULL,
             person_id INTEGER NOT NULL,
             FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
             FOREIGN KEY (person_id) REFERENCES persons(id) ON DELETE CASCADE,
             PRIMARY KEY (film_id, person_id)
-        );
+        )"#,
+        [],
+    )?;
 
+    conn.execute(
+        r#"--sql
         CREATE TABLE IF NOT EXISTS film_characters (
             film_id INTEGER NOT NULL,
             character_id INTEGER NOT NULL,
             FOREIGN KEY (film_id) REFERENCES films(id) ON DELETE CASCADE,
             FOREIGN KEY (character_id) REFERENCES characters(id) ON DELETE CASCADE,
             PRIMARY KEY (film_id, character_id)
-        );
-        "#,
+        )"#,
+        [],
     )?;
 
     Ok(())
