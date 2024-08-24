48cbc94bb2cadf12fa53fadcdf3d5ce4b9c47a9d
Henning Cullin <henningcullin@gmail.com>
2024-08-24 16:09:23 +0200

    feat: use react-query

--- src/films/edit-film/SelectFilmPopup.tsx
+++ src/films/edit-film/SelectFilmPopup.tsx
@@ -21,6 +21,7 @@ import { Input } from '@/components/ui/input';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 import { shell } from '@tauri-apps/api';
+import { useQuery } from '@tanstack/react-query';
 
 interface Arguments {
   onSelect: (url: string | undefined) => void;
@@ -38,59 +39,56 @@ interface SearchItem {
   y: number;
 }
 
-async function searchFilms(
-  searchValue: string
-): Promise<SearchItem[] | boolean> {
-  try {
-    const url = `https://v3.sg.media-imdb.com/suggestion/x/${searchValue}.json?includeVideos=0`;
-    const response: string = await invoke('fetch_data', { url });
-    const parsed: {
-      d: SearchItem[];
-    } = JSON.parse(response);
-    const items = parsed.d;
-    const filmItems = items.filter((item) => item.id.includes('tt'));
-    return filmItems;
-  } catch (error) {
-    console.error('Error searching for films', error);
-    return false;
-  }
+/**
+ * Get films from imdb
+ */
+async function searchFilms(searchValue: string): Promise<SearchItem[]> {
+  const url = `https://v3.sg.media-imdb.com/suggestion/x/${searchValue}.json?includeVideos=0`;
+  const response: string = await invoke('fetch_data', { url });
+  const parsed: { d: SearchItem[] } = JSON.parse(response);
+  const items = parsed.d;
+  const filmItems = items.filter((item) => item.id.includes('tt'));
+  return filmItems;
 }
-
+/**
+ * Sanitze filenames in attempt to get a search term for the film
+ */
 function getFilmName(filePath: string | undefined): string {
   const fileName = filePath?.split('\\')?.pop();
-  const value =
-    fileName
-      ?.replace(/\.\d{4}.*\.(mp4|mkv)$/, '')
-      // @ts-ignore
-      ?.replaceAll('.', ' ') ?? '';
+  const value = // @ts-ignore
+    fileName?.replace(/\.\d{4}.*\.(mp4|mkv)$/, '')?.replaceAll('.', ' ') ?? '';
   return value;
 }
 
 function SelectFilmPopup({ onSelect, filePath }: Arguments) {
   const [open, setOpen] = useState<boolean>(false);
-  const [searchItems, setSearchItems] = useState<SearchItem[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>(
+    getFilmName(filePath) || ''
+  );
 
-  const initialSearchTerm = getFilmName(filePath);
+  const {
+    data: searchItems,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<SearchItem[], Error>({
+    queryKey: ['searchFilms', searchTerm],
+    queryFn: () => searchFilms(searchTerm),
+    enabled: !!searchTerm, // Only run query if searchTerm is not empty
+  });
 
-  useEffect(() => {
-    search(initialSearchTerm);
-  }, [initialSearchTerm]);
-
-  async function handleSearch(event: React.FormEvent<HTMLFormElement>) {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
-    const searchValue = formData.get('searchValue');
-    if (typeof searchValue !== 'string') return;
-    search(searchValue);
+    const searchValue = formData.get('searchValue') as string;
+    if (searchValue) {
+      setSearchTerm(searchValue);
+    }
   }
 
-  async function search(searchValue: string) {
-    if (!searchValue?.length) return;
-    const items = await searchFilms(searchValue);
-
-    if (typeof items === 'boolean') return;
-    setSearchItems(items);
-  }
+  useEffect(() => {
+    setSearchTerm(getFilmName(filePath));
+  }, [filePath]);
 
   const handleSelect = useCallback(
     (id: string) => {
@@ -120,23 +118,31 @@ function SelectFilmPopup({ onSelect, filePath }: Arguments) {
         <DialogHeader>
           <DialogTitle>Select film</DialogTitle>
         </DialogHeader>
-        <form onSubmit={handleSearch} className='flex'>
+        <form onSubmit={onSubmit} className='flex'>
           <Input
             name='searchValue'
             placeholder='Enter a film title'
             type={'search'}
-            defaultValue={initialSearchTerm}
+            defaultValue={searchTerm}
           />
           <Button variant='secondary' className='ml-1'>
             Search
             <MagnifyingGlassIcon className='ml-1' />
           </Button>
         </form>
-        <FilmList
-          films={searchItems}
-          handleSelect={handleSelect}
-          handleLink={handleLink}
-        ></FilmList>
+        {isLoading ? (
+          <b>Loading...</b>
+        ) : isError ? (
+          <b>{error?.message}</b>
+        ) : searchItems?.length ? (
+          <FilmList
+            films={searchItems}
+            handleSelect={handleSelect}
+            handleLink={handleLink}
+          ></FilmList>
+        ) : (
+          <b>No films were found</b>
+        )}
         <DialogFooter>
           <DialogClose asChild>
             <Button type='button' variant='secondary'>
