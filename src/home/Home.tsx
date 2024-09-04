cbd8c28e0d1f3163d9a9d769c2a3dc7273f45acb
Henning Cullin <henningcullin@gmail.com>
2024-09-04 19:08:47 +0200

    feat: show film grid

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,44 +1,51 @@
-import { filmAtom } from '@/lib/atoms';
+import Cover from '@/components/Cover';
 import { Film } from '@/lib/types';
 import { invoke } from '@tauri-apps/api/tauri';
-import { useAtom } from 'jotai';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  const [films, setFilms] = useAtom(filmAtom);
-
-  const { error, isLoading } = useQuery<Film[], Error>(
-    'films',
-    async () => {
+  const {
+    data: films,
+    error,
+    isError,
+    isLoading,
+  } = useQuery<Film[], Error>({
+    queryKey: ['films'],
+    queryFn: async () => {
       const data = await invoke<Film[]>('get_all_films');
       return data || [];
     },
-    {
-      onSuccess: (data) => {
-        setFilms(data);
-      },
-    }
-  );
+  });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
+  if (isError) {
     return <div>Error fetching films: {error.message}</div>;
   }
 
   return (
-    <div>
-      {films
-        .filter((film) => film.registered)
-        .map((film) => (
-          <div key={film.id}>
-            <h2>{film.title}</h2>
-            <p>File: {film.file}</p>
-          </div>
-        ))}
+    <div className='grid grid-cols-4 p-4'>
+      {isLoading ? (
+        <Skeleton className='w-[375px] h-[525px]'></Skeleton>
+      ) : !films ? (
+        <b>No films found</b>
+      ) : (
+        films.filter((film) => film.registered).map(FilmItem)
+      )}
     </div>
+  );
+}
+
+function FilmItem(film: Film) {
+  return (
+    <Link
+      to={`/film/${film.id}`}
+      key={film.id}
+      className='w-[375px] h-[525px] cursor-pointer'
+    >
+      <h1 className=''>{film.title}</h1>
+      <Cover id={film.id}></Cover>
+    </Link>
   );
 }
 
