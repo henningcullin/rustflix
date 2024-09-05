c187d5831e05861f3badc9d968ff6f232a63f051
Henning Cullin <henningcullin@gmail.com>
2024-09-05 20:42:38 +0200

    ci: remove jotai

--- src/lib/atoms.ts
+++
@@ -1,7 +0,0 @@
-import { Directory } from '@/directories/Directories';
-import { atom } from "jotai";
-import { Film } from './types';
-
-export const directoryAtom = atom<Directory[]>([]);
-
-export const filmAtom = atom<Film[]>([]);
