3f9dfc48d10b01d7da18ad45ec8230698f811a56
Henning Cullin <henningcullin@gmail.com>
2024-09-05 20:17:56 +0200

    refactor: switch to reactQuery

--- src/films/Films.tsx
+++ src/films/Films.tsx
@@ -1,5 +1,4 @@
 import { invoke } from '@tauri-apps/api/tauri';
-import { useEffect } from 'react';
 import {
   Table,
   TableBody,
@@ -10,66 +9,94 @@ import {
 } from '@/components/ui/table';
 import { CheckIcon, Cross2Icon, Pencil2Icon } from '@radix-ui/react-icons';
 
-import { useAtom } from 'jotai';
-import { filmAtom } from '@/lib/atoms';
 import { Link } from 'react-router-dom';
-export interface Film {
-  id: number;
-  file: string;
-  directory: { id: number; path: string };
-  link?: string;
-  title?: string;
-  synopsis?: string;
-  release_date?: Date;
-  duration?: number;
-  cover_image?: string;
-  registered: boolean;
-}
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Button } from '@/components/ui/button';
+import { Film } from '@/lib/types';
 
 function Films() {
-  const [films, setFilms] = useAtom(filmAtom);
+  const queryClient = useQueryClient();
 
-  async function getFilms() {
+  async function syncNewFilms() {
     try {
-      const data: Film[] | null = await invoke('get_all_films');
-      console.log(data);
-      if (data) setFilms(data);
+      // Invoke the Tauri command to sync new films
+      await invoke('sync_new_films');
+      // Invalidate the films query to refetch the data
+      queryClient.invalidateQueries({ queryKey: ['films'] });
     } catch (error) {
-      console.error('Failed to fetch films:', error);
+      console.error('Error syncing films:', error);
     }
   }
 
-  useEffect(() => {
-    getFilms();
-  }, []);
+  const {
+    data: films,
+    error,
+    isError,
+    isLoading,
+  } = useQuery<Film[], Error>({
+    queryKey: ['films'],
+    queryFn: async () => {
+      const data = await invoke<Film[]>('get_all_films');
+      return data || [];
+    },
+  });
+
+  if (isError) return <div>{error.message}</div>;
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Directory</TableHead>
-          <TableHead>Path</TableHead>
-          <TableHead>Registered</TableHead>
-          <TableHead>Action</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {films.map((film) => (
+    <>
+      <div>
+        <Button onClick={syncNewFilms}>Sync new films</Button>
+      </div>
+      <Table>
+        <TableHeader>
           <TableRow>
-            <TableCell>{film.directory.id}</TableCell>
-            <TableCell>{film.file}</TableCell>
-            <TableCell>
-              <CheckBox state={film.registered} />
-            </TableCell>
-            <TableCell>
-              <Link to={`/film/edit/${film.id}`}>
-                <Pencil2Icon /> Edit
-              </Link>
-            </TableCell>
+            <TableHead>Directory</TableHead>
+            <TableHead>Path</TableHead>
+            <TableHead>Registered</TableHead>
+            <TableHead>Action</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {isLoading ? (
+            <TableRow>
+              <TableCell>
+                <Skeleton />
+              </TableCell>
+              <TableCell>
+                <Skeleton />
+              </TableCell>
+              <TableCell>
+                <Skeleton />
+              </TableCell>
+              <TableCell>
+                <Skeleton />
+              </TableCell>
+            </TableRow>
+          ) : films ? (
+            films.map((film) => (
+              <TableRow>
+                <TableCell>{film.directory.id}</TableCell>
+                <TableCell>{film.file}</TableCell>
+                <TableCell>
+                  <CheckBox state={film.registered} />
+                </TableCell>
+                <TableCell>
+                  <Link to={`/film/edit/${film.id}`}>
+                    <Pencil2Icon /> Edit
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell>No films found</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
   );
 }
 
