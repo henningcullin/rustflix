e01fff611bd0b4faccf13a197e6a3ff79b6afb65
Henning Cullin <henningcullin@gmail.com>
2024-09-13 19:57:05 +0200

    deps: toast component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { Toaster } from './components/ui/toaster';
 
 function Layout() {
   return (
     <>
       <Sidebar />
       <Outlet />
+      <Toaster />
     </>
   );
 }
