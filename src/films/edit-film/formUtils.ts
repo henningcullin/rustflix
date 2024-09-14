4f7dff4177a1f324d2ddb0288bf4b87e42eca222
Henning Cullin <henningcullin@gmail.com>
2024-09-14 21:13:40 +0200

    refactor: remove registered from form

--- src/films/edit-film/formUtils.ts
+++ src/films/edit-film/formUtils.ts
@@ -1,21 +1,20 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-    imdb_id: z.string(),
-    title: z.string(),
-    release_date: z.date(),
-    plot: z.string(),
-    run_time: z.number(),
-    has_color: z.boolean(),
-    rating: z.number(),
-    has_watched: z.boolean(),
-    left_off_point: z.number().optional(),
-    registered: z.boolean().optional(),
-    genres: z.array(z.number()),
-    directors: z.array(z.number()),
-    stars: z.array(z.number()),
-    languages: z.array(z.number()),
-    keywords: z.array(z.string()),
-  });
-  
-  export type FormSchema = z.infer<typeof formSchema>;
+  imdb_id: z.string(),
+  title: z.string(),
+  release_date: z.date(),
+  plot: z.string(),
+  run_time: z.number(),
+  has_color: z.boolean(),
+  rating: z.number(),
+  has_watched: z.boolean(),
+  left_off_point: z.number().optional(),
+  genres: z.array(z.number()),
+  directors: z.array(z.number()),
+  stars: z.array(z.number()),
+  languages: z.array(z.number()),
+  keywords: z.array(z.string()),
+});
+
+export type FormSchema = z.infer<typeof formSchema>;
