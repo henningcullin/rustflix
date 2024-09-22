8aea7ae846940ea83d281816817e9d9d564e62af
Henning Cullin <henningcullin@gmail.com>
2024-09-22 18:12:30 +0200

    feat: get all persons action

---
+++ src-tauri/src/persons/actions.rs
@@ -0,0 +1,26 @@
+use crate::{database::create_connection, error::AppError};
+
+use super::Person;
+
+pub fn get_all_persons() -> Result<Vec<Person>, AppError> {
+    let conn = create_connection()?;
+
+    let mut stmt = conn.prepare(
+        r#"--sql
+        SELECT 
+            id,  
+            imdb_id,  
+            name,  
+            age,  
+            gender,  
+            birthplace
+        FROM
+            persons"#,
+    )?;
+
+    let persons = stmt
+        .query_map([], |row| Person::from_row(row))?
+        .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(persons)
+}
