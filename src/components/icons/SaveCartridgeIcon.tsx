e7acc33096acbfc958ae835f76d61ac9da22b4d2
Henning Cullin <henningcullin@gmail.com>
2024-09-22 22:10:15 +0200

    refactor: fix icon

--- src/components/icons/SaveCartridgeIcon.tsx
+++ src/components/icons/SaveCartridgeIcon.tsx
@@ -1,48 +1,22 @@
 import * as React from 'react';
 
-interface SaveCartridgeIconProps extends React.SVGProps<SVGSVGElement> {
-  title?: string;
-}
-
 const SaveCartridgeIcon = React.forwardRef<
   SVGSVGElement,
-  SaveCartridgeIconProps
->(function SaveCartridgeIcon({ title = 'Save Cartridge Icon', ...props }, ref) {
+  React.SVGProps<SVGSVGElement>
+>(({ className, ...props }, ref) => {
   return (
     <svg
       viewBox='0 0 24 24'
-      fill='none' // Set fill to none to emphasize the outline style
-      stroke='currentColor' // Stroke color will be used for outlines
-      strokeWidth='1.5' // Thinner stroke for a lighter outline
-      height={props.height || '1em'}
-      width={props.width || '1em'}
+      fill='currentColor'
+      height='1em'
+      width='1em'
       ref={ref}
-      aria-hidden={title ? 'false' : 'true'}
-      role={title ? 'img' : 'presentation'}
-      {...props}
+      className={className} // Make sure className is passed to apply styles
+      {...props} // Spread remaining props (e.g., onClick, role)
     >
-      {title && <title>{title}</title>}
       <path
-        d='M3.75 2A1.75 1.75 0 002 3.75v16.5A1.75 1.75 0 003.75 22h16.5A1.75 1.75 0 0022 20.25V7.56c0-.465-.185-.912-.513-1.24l-3.807-3.807A1.75 1.75 0 0015.44 2H3.75z'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-      <rect
-        x='7.25'
-        y='10'
-        width='9'
-        height='7.5'
-        rx='0.75'
-        ry='0.75'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-      <path d='M10 4.75v3.5' strokeLinecap='round' strokeLinejoin='round' />
-      <path
-        d='M17.5 10h-7v4.5h7V10z'
-        fill='none'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        fillRule='evenodd'
+        d='M3.75 2A1.75 1.75 0 002 3.75v16.5A1.75 1.75 0 003.75 22h16.5A1.75 1.75 0 0022 20.25V7.56c0-.465-.185-.912-.513-1.24l-3.807-3.807A1.75 1.75 0 0015.44 2H3.75zM3.5 3.75c0-.138.112-.25.25-.25h11.69c.066 0 .13.026.177.073l3.807 3.807a.25.25 0 01.073.177v12.693a.25.25 0 01-.25.25H3.75a.25.25 0 01-.25-.25V3.75zm14 7.5a.75.75 0 00-.75-.75h-8.5a.75.75 0 00-.75.75v6a.75.75 0 00.75.75h8.5a.75.75 0 00.75-.75v-6zm-8 1.5h7v4.5h-7v-4.5zm3-8a.5.5 0 00-1 0v3.5h1V4.75z'
       />
     </svg>
   );
