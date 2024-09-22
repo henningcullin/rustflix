ec59655b5ce85c6c2cdbd1767421f0f1069842ad
Henning Cullin <henningcullin@gmail.com>
2024-09-22 22:13:10 +0200

    feat: edit dialog

--- src/films/edit-film/CharacterTable.tsx
+++ src/films/edit-film/CharacterTable.tsx
@@ -31,24 +31,41 @@ import {
   Pencil2Icon,
   TrashIcon,
 } from '@radix-ui/react-icons';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api';
 import { useToast } from '@/hooks/use-toast';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
+  DialogOverlay,
   DialogTitle,
 } from '@/components/ui/dialog';
 import SaveCartridgeIcon from '@/components/icons/SaveCartridgeIcon';
+import { z } from 'zod';
+import { i32 } from '@/lib/utils';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import PersonBox from '@/components/PersonBox';
 
 // Encapsulated Delete Logic Hook
 function useCharacterDelete(film: Film | undefined) {
-  const [isDeleteOpen, setIsDeleteOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
     null
   );
@@ -85,18 +102,18 @@ function useCharacterDelete(film: Film | undefined) {
   const handleDelete = useCallback(() => {
     if (selectedCharacter) {
       deleteCharacterMutation.mutate(selectedCharacter);
-      setIsDeleteOpen(false);
+      setOpen(false);
     }
   }, [selectedCharacter, deleteCharacterMutation]);
 
   const characterDelete = useCallback((character: Character) => {
     setSelectedCharacter(character);
-    setIsDeleteOpen(true);
+    setOpen(true);
   }, []);
 
   const DeleteDialog = useCallback(() => {
     return (
-      <Dialog open={isDeleteOpen} onOpenChange={setIsDeleteOpen}>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent>
           <DialogHeader>
             <DialogTitle>Confirm Deletion</DialogTitle>
@@ -106,10 +123,12 @@ function useCharacterDelete(film: Film | undefined) {
             </DialogDescription>
           </DialogHeader>
           <DialogFooter>
-            <Button variant='outline' onClick={() => setIsDeleteOpen(false)}>
-              <Cross2Icon className='w-5 h-5 mr-2' />
-              Cancel
-            </Button>
+            <DialogClose>
+              <Button type='button' variant='secondary'>
+                <Cross2Icon className='w-5 h-5 mr-2' />
+                Cancel
+              </Button>
+            </DialogClose>
             <Button
               variant='destructive'
               onClick={() => {
@@ -123,119 +142,193 @@ function useCharacterDelete(film: Film | undefined) {
         </DialogContent>
       </Dialog>
     );
-  }, [isDeleteOpen, selectedCharacter, handleDelete]);
+  }, [open, selectedCharacter, handleDelete]);
 
   return { characterDelete, DeleteDialog };
 }
 
-function CharacterTable({ film }: { film: Film | undefined }) {
-  const [isEditOpen, setIsEditOpen] = useState<boolean>(false);
+function useCharacterEdit(film: Film | undefined) {
+  const [open, setOpen] = useState<boolean>(false);
   const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
     null
   );
+  const queryClient = useQueryClient();
 
-  // Use the custom hook for character deletion
-  const { characterDelete, DeleteDialog } = useCharacterDelete(film);
+  const formSchema = z.object({
+    description: z.string().min(1, 'A character requires a description'),
+    actor: i32('A character requires an actor'),
+  });
 
-  const openEditDialog = useCallback((character: Character) => {
-    setSelectedCharacter(character);
-    setIsEditOpen(true);
+  type FormSchema = z.infer<typeof formSchema>;
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: '',
+      actor: undefined,
+    },
+  });
+
+  const { reset } = form;
+
+  useEffect(() => {
+    reset({
+      description: selectedCharacter?.description,
+      actor: selectedCharacter?.actor.id,
+    });
+  }, [selectedCharacter]);
+
+  const onSuccess = useCallback((values: FormSchema) => {
+    console.log(values);
   }, []);
 
-  return (
-    <>
-      {/* Render the delete dialog from the hook */}
-      <DeleteDialog />
+  const characterEdit = useCallback((character: Character) => {
+    setSelectedCharacter(character);
+    setOpen(true);
+  }, []);
 
-      <Dialog open={isEditOpen} onOpenChange={setIsEditOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Edit Character</DialogTitle>
-            <DialogDescription>
-              Editing character {selectedCharacter?.description}
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button variant='outline' onClick={() => setIsEditOpen(false)}>
-              <Cross2Icon className='w-5 h-5 mr-2' />
-              Cancel
-            </Button>
-            <Button type='submit'>
-              <SaveCartridgeIcon className='w-5 h-5 mr-2' />
-              Save
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-
-      <Table>
-        <TableCaption>List of all characters in the film</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Avatar</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Actor</TableHead>
-            <TableHead className='w-12'>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {film?.stars?.map((character) => (
-            <TableRow key={character.actor.id}>
-              <TableCell>
-                <Avatar id={character.actor.id}></Avatar>
-              </TableCell>
-              <TableCell>{character.description}</TableCell>
-              <TableCell>
-                <HoverCard>
-                  <HoverCardTrigger>
-                    <span className='hover:underline underline-offset-4 cursor-pointer'>
-                      {character.actor.name}
-                    </span>
-                  </HoverCardTrigger>
-                  <HoverCardContent>
-                    <b>{character.actor.id}</b>
-                    <br />
-                    <i>{character.actor.imdb_id}</i>
-                  </HoverCardContent>
-                </HoverCard>
-              </TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <Button variant='outline' className='w-10 h-10 p-0'>
-                      <DotsHorizontalIcon />
+  const EditDialog = useCallback(() => {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogOverlay>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Edit Character</DialogTitle>
+              <DialogDescription>
+                Editing {selectedCharacter?.description}
+              </DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSuccess)}>
+                <FormField
+                  control={form.control}
+                  name='description'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        The description of the character
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='actor'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Actor</FormLabel>
+                      <FormControl>
+                        <PersonBox {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        The actor playing the character
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <DialogFooter>
+                  <DialogClose>
+                    <Button variant='secondary'>
+                      <Cross2Icon className='w-5 h-5 mr-2' />
+                      Cancel
                     </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <EyeOpenIcon className='w-5 h-5 mr-2' />
-                      View
+                  </DialogClose>
+                  <Button type='submit'>
+                    <SaveCartridgeIcon className='w-5 h-5 mr-2' />
+                    Save
+                  </Button>
+                </DialogFooter>
+              </form>
+            </Form>
+            <PersonBox value='' onChange={() => {}} />
+          </DialogContent>
+        </DialogOverlay>
+      </Dialog>
+    );
+  }, [open, selectedCharacter]);
+
+  return { characterEdit, EditDialog };
+}
+
+function CharacterTable({ film }: { film: Film | undefined }) {
+  // Use the custom hook for character deletion
+  const { characterDelete, DeleteDialog } = useCharacterDelete(film);
+  const { characterEdit, EditDialog } = useCharacterEdit(film);
+
+  return (
+    <Table>
+      <DeleteDialog />
+      <EditDialog />
+      <TableCaption>List of all characters in the film</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Avatar</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead>Actor</TableHead>
+          <TableHead className='w-12'>Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {film?.stars?.map((character) => (
+          <TableRow key={character.actor.id}>
+            <TableCell>
+              <Avatar id={character.actor.id}></Avatar>
+            </TableCell>
+            <TableCell>{character.description}</TableCell>
+            <TableCell>
+              <HoverCard>
+                <HoverCardTrigger>
+                  <span className='hover:underline underline-offset-4 cursor-pointer'>
+                    {character.actor.name}
+                  </span>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <b>{character.actor.id}</b>
+                  <br />
+                  <i>{character.actor.imdb_id}</i>
+                </HoverCardContent>
+              </HoverCard>
+            </TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant='outline' className='w-10 h-10 p-0'>
+                    <DotsHorizontalIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <EyeOpenIcon className='w-5 h-5 mr-2' />
+                    View
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem onClick={() => characterEdit(character)}>
+                      <Pencil2Icon className='w-5 h-5 mr-2' />
+                      Edit
                     </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem
-                        onClick={() => openEditDialog(character)}
-                      >
-                        <Pencil2Icon className='w-5 h-5 mr-2' />
-                        Edit
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        onClick={() => characterDelete(character)}
-                      >
-                        <TrashIcon className='w-5 h-5 mr-2' />
-                        Delete
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
+                    <DropdownMenuItem
+                      onClick={() => characterDelete(character)}
+                    >
+                      <TrashIcon className='w-5 h-5 mr-2' />
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
 
