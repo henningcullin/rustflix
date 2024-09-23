14b729995d83dbc8468f9da27d884dbc2b7cb2d2
Henning Cullin <henningcullin@gmail.com>
2024-09-23 21:27:11 +0200

    feat: update character action

--- src-tauri/src/characters/actions.rs
+++ src-tauri/src/characters/actions.rs
@@ -14,3 +14,19 @@ pub fn delete_character(film_id: i32, actor: i32) -> Result<usize, AppError> {
 
     Ok(rows_affected)
 }
+
+pub fn update_character(
+    film_id: i32,
+    actor: i32,
+    new_description: &String,
+    new_actor: i32,
+) -> Result<usize, AppError> {
+    let conn = create_connection()?;
+
+    let rows_affected = conn.execute(
+        "UPDATE characters SET description = ?1, actor = ?2 WHERE film_id = ?3 AND actor = ?4",
+        params![new_description, new_actor, film_id, actor],
+    )?;
+
+    Ok(rows_affected)
+}
