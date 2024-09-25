472956898d432aadd1e1d13b25e522c4df33f11b
Henning Cullin <henningcullin@gmail.com>
2024-09-25 20:18:55 +0200

    feat: useCharacterCreate hook

---
+++ src/films/edit-film/CharacterTable/useCharacterCreate.tsx
@@ -0,0 +1,166 @@
+import { useState, useCallback } from 'react';
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { invoke } from '@tauri-apps/api';
+import { toast } from '@/hooks/use-toast';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import SaveCartridgeIcon from '@/components/icons/SaveCartridgeIcon';
+import { z } from 'zod';
+import { i32 } from '@/lib/utils';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import PersonBox from '@/components/PersonBox';
+import { Film } from '@/lib/types';
+import { Button } from '@/components/ui/button';
+import { Cross2Icon } from '@radix-ui/react-icons';
+
+function useCharacterCreate(film: Film | undefined) {
+  const [open, setOpen] = useState<boolean>(false);
+  const queryClient = useQueryClient();
+
+  const formSchema = z.object({
+    description: z.string().min(1, 'A character requires a description'),
+    actor: i32('A character requires an actor'),
+  });
+
+  type FormSchema = z.infer<typeof formSchema>;
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: '',
+      actor: undefined,
+    },
+  });
+
+  const createCharacterMutation = useMutation({
+    mutationFn: async (formValues: FormSchema) => {
+      await invoke('create_character', {
+        filmId: film?.id,
+        actorId: formValues.actor,
+        description: formValues.description,
+      });
+    },
+    onError: (error) => {
+      console.error(error);
+      toast({
+        variant: 'destructive',
+        title: 'Failed to create the character',
+        description: error.message,
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: 'Character created',
+        description: `Character was successfully created`,
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['film', film?.id?.toString()],
+      });
+      queryClient.invalidateQueries({ queryKey: ['films'] });
+    },
+  });
+
+  const { reset } = form;
+
+  const onSuccess = useCallback((values: FormSchema) => {
+    createCharacterMutation.mutate(values);
+  }, []);
+
+  const characterCreate = useCallback(() => {
+    reset({
+      description: '',
+      actor: undefined,
+    });
+    setOpen(true);
+  }, []);
+
+  const CreateDialog = useCallback(() => {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogOverlay>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create character</DialogTitle>
+              <DialogDescription>Creating character</DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSuccess)}>
+                <FormField
+                  control={form.control}
+                  name='description'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        The description of the character
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='actor'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Actor</FormLabel>
+                      <FormControl>
+                        <PersonBox {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        The actor playing the character
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <DialogFooter>
+                  <DialogClose>
+                    <Button variant='secondary'>
+                      <Cross2Icon className='w-5 h-5 mr-2' />
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button
+                    type='submit'
+                    disabled={createCharacterMutation.isPending}
+                  >
+                    <SaveCartridgeIcon className='w-5 h-5 mr-2' />
+                    Save
+                  </Button>
+                </DialogFooter>
+              </form>
+            </Form>
+          </DialogContent>
+        </DialogOverlay>
+      </Dialog>
+    );
+  }, [open]);
+
+  return { characterCreate, CreateDialog };
+}
+
+export default useCharacterCreate;
