72bac175b1907b7e804331adfa1ebcea934a63e0
Henning Cullin <henningcullin@gmail.com>
2024-09-28 10:28:49 +0200

    refactor: fix title

--- src/films/edit-film/KeywordForm.tsx
+++ src/films/edit-film/KeywordForm.tsx
@@ -90,7 +90,7 @@ function KeywordForm({ film }: { film: Film | undefined }) {
 
   return (
     <div className='max-w-md mx-auto p-6 bg-background rounded-lg shadow'>
-      <h2 className='text-2xl font-bold mb-4'>Movie Keywords</h2>
+      <h2 className='text-2xl font-bold mb-4'>Keywords</h2>
       <div className='mb-4'>
         <div className='flex flex-wrap gap-2'>
           {film?.keywords?.map((keyword) => (
