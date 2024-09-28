149a4b02479fce1d07016c83bbe2cc2eb7a69410
Henning Cullin <henningcullin@gmail.com>
2024-09-28 09:11:15 +0200

    feat: delete keyword action

--- src-tauri/src/keywords/actions.rs
+++ src-tauri/src/keywords/actions.rs
@@ -12,3 +12,14 @@ pub fn create_keyword(film_id: i32, keyword: &String) -> Result<usize, AppError>
 
     Ok(rows_affected)
 }
+
+pub fn delete_keyword(film_id: i32, keyword: &String) -> Result<usize, AppError> {
+    let conn = create_connection()?;
+
+    let rows_affected = conn.execute(
+        "DELETE FROM film_keywords WHERE film_id = ?1 AND keyword = ?2",
+        params![film_id, keyword],
+    )?;
+
+    Ok(rows_affected)
+}
