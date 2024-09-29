13b29e6f47e046440e9c7275a0ccf94892bc34aa
Henning Cullin <henningcullin@gmail.com>
2024-09-29 21:50:25 +0200

    feat: language fromrow

--- src-tauri/src/languages/models.rs
+++ src-tauri/src/languages/models.rs
@@ -1,7 +1,18 @@
 use serde::Serialize;
 
+use crate::FromRow;
+
 #[derive(Debug, Serialize)]
 pub struct Language {
     pub id: u32,
     pub name: String,
+}
+
+impl FromRow for Language {
+    fn from_row(row: &rusqlite::Row) -> Result<Self, rusqlite::Error> {
+        Ok(Language {
+            id: row.get("id")?,
+            name: row.get("name")?,
+        })
+    }
 }
