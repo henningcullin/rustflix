f8b7da6887bc6f3b552c012660f102887f70624c
Henning Cullin <henningcullin@gmail.com>
2024-10-18 20:41:20 +0200

    feat: add and remove genre from film actions

--- src-tauri/src/genres/actions.rs
+++ src-tauri/src/genres/actions.rs
@@ -1,3 +1,5 @@
+use rusqlite::params;
+
 use crate::{database::create_connection, error::AppError, FromRow};
 
 use super::Genre;
@@ -12,4 +14,26 @@ pub fn get_all_genres() -> Result<Vec<Genre>, AppError> {
         .collect::<Result<Vec<_>, _>>()?;
 
     Ok(genres)
+}
+
+pub fn add_genre_to_film(film_id: i32, genre_id: &i32) -> Result<usize, AppError> {
+    let conn = create_connection()?;
+
+    let rows_affected = conn.execute(
+        "INSERT INTO film_genres (film_id, genre_id) VALUES (?1, ?2)",
+        params![film_id, genre_id],
+    )?;
+
+    Ok(rows_affected)
+}
+
+pub fn remove_genre_from_film(film_id: i32, genre_id: &i32) -> Result<usize, AppError> {
+    let conn = create_connection()?;
+
+    let rows_affected = conn.execute(
+        "DELETE FROM film_genres WHERE film_id = ?1 AND genre_id = ?2",
+        params![film_id, genre_id],
+    )?;
+
+    Ok(rows_affected)
 }
