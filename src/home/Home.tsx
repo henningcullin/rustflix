e6e43d598b1962ec5f365aaf6dd81afaf192940d
Henning Cullin <henningcullin@gmail.com>
2024-10-20 19:09:36 +0200

    refactor: new tailwind approach

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -24,27 +24,36 @@ function Home() {
   }
 
   return (
-    <div className='inline-flex flex-wrap gap-24 p-6'>
+    <div className='grid gap-6 p-6 gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
       {isLoading ? (
-        <Skeleton className='w-[375px] h-[525px]'></Skeleton>
+        Array.from({ length: 5 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            className='aspect-[187,5/262,5] w-full rounded-lg'
+          />
+        ))
       ) : !films ? (
         <b>No films found</b>
       ) : (
-        films.filter((film) => film.registered).map(FilmItem)
+        films
+          .filter((film) => film.registered)
+          .map((film) => <FilmItem key={film.id} film={film} />)
       )}
     </div>
   );
 }
 
-function FilmItem(film: Film) {
+function FilmItem({ film }: { film: Film }) {
   return (
     <Link
       to={`/film/${film.id}`}
       key={film.id}
-      className='w-[375px] h-[525px] cursor-pointer'
+      className='aspect-[187,5/262,5] cursor-pointer transform transition-transform duration-300 hover:scale-105'
     >
-      <h1 className=''>{film.title}</h1>
       <Cover id={film.id}></Cover>
+      <h1 className='mt-2 text-center text-lg font-medium truncate'>
+        {film.title}
+      </h1>
     </Link>
   );
 }
