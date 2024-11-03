cc25fdf1db9ccbedf74bf7cceace7a413d50b922
Henning Cullin <henningcullin@gmail.com>
2024-11-03 18:04:22 +0100

    refactor: improve avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,31 +1,45 @@
-import React from 'react';
+import { forwardRef, memo } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
 import { useQuery } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api/tauri';
+import { PersonIcon } from '@radix-ui/react-icons';
 
-interface AvatarProps {
+type AvatarProps = {
   id: number;
-}
+};
 
-function Avatar({ id }: AvatarProps): React.ReactElement {
-  const {
-    data: src,
-    error,
-    isLoading,
-  } = useQuery<string, Error>({
-    queryKey: ['avatar', id],
-    queryFn: () => invoke('get_avatar', { id }),
-  });
+const Avatar = memo(
+  forwardRef<HTMLDivElement, AvatarProps>(({ id }, ref) => {
+    const {
+      data: src,
+      error,
+      isLoading,
+    } = useQuery<string, Error>({
+      queryKey: ['avatar', id],
+      queryFn: () => invoke('get_avatar', { id }),
+    });
 
-  if (isLoading) {
-    return <Skeleton className='rounded-full w-[64px] h-[64px]' />;
-  }
+    // Define a base class for the avatar
+    const baseClass =
+      'rounded-full w-[64px] h-[64px] flex items-center justify-center bg-gray-200';
 
-  if (error) {
-    return <div className='rounded-full w-[64px] h-[64px]' />;
-  }
+    // Container for the avatar, skeleton, or error icon
+    return (
+      <div ref={ref} className={baseClass}>
+        {isLoading ? (
+          <Skeleton className='rounded-full w-full h-full' />
+        ) : error ? (
+          <PersonIcon className='w-1/2 h-1/2 text-gray-500' /> // Icon for error state
+        ) : (
+          <img
+            src={src || ''}
+            alt='User Avatar'
+            className='rounded-full object-cover w-full h-full'
+          />
+        )}
+      </div>
+    );
+  })
+);
 
-  return <img src={src || ''} className='rounded-full w-[64px] h-[64px]' />;
-}
-
-export default Avatar;
+export default memo(Avatar);
