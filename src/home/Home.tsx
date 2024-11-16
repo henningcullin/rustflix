369801cf4c826ab177383f55a96489d837d5e0e1
Henning Cullin <henningcullin@gmail.com>
2024-11-16 23:57:22 +0100

    refactor: filter accordion

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -4,6 +4,12 @@ import { invoke } from '@tauri-apps/api/tauri';
 import { useQuery } from '@tanstack/react-query';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Link } from 'react-router-dom';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 function Home() {
   const {
@@ -24,22 +30,30 @@ function Home() {
   }
 
   return (
-    <div className='grid gap-6 p-6 gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-      {isLoading ? (
-        Array.from({ length: 5 }).map((_, index) => (
-          <Skeleton
-            key={index}
-            className='aspect-[187,5/262,5] w-full rounded-lg'
-          />
-        ))
-      ) : !films ? (
-        <b>No films found</b>
-      ) : (
-        films
-          .filter((film) => film.registered)
-          .map((film) => <FilmItem key={film.id} film={film} />)
-      )}
-    </div>
+    <>
+      <Accordion type='single' collapsible className='w-full'>
+        <AccordionItem value='filters'>
+          <AccordionTrigger>Filters</AccordionTrigger>
+          <AccordionContent>Add Filters</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <div className='grid gap-6 p-6 gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+        {isLoading ? (
+          Array.from({ length: 5 }).map((_, index) => (
+            <Skeleton
+              key={index}
+              className='aspect-[187,5/262,5] w-full rounded-lg'
+            />
+          ))
+        ) : !films ? (
+          <b>No films found</b>
+        ) : (
+          films
+            .filter((film) => film.registered)
+            .map((film) => <FilmItem key={film.id} film={film} />)
+        )}
+      </div>
+    </>
   );
 }
 
@@ -47,7 +61,6 @@ function FilmItem({ film }: { film: Film }) {
   return (
     <Link
       to={`/film/${film.id}`}
-      key={film.id}
       className='aspect-[187,5/262,5] cursor-pointer transform transition-transform duration-300 hover:scale-105'
     >
       <Cover id={film.id}></Cover>
