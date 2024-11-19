4f7353fd1c50731ef95a15457adcc376f23d9565
Henning Cullin <henningcullin@gmail.com>
2024-11-19 21:56:31 +0100

    refactor: added aliases and green variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,6 +4,12 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
+const greenStyle =
+  'bg-green-500 text-white shadow-sm hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700 dark:text-green-100';
+
+const redStyle =
+  'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90';
+
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
@@ -11,14 +17,16 @@ const buttonVariants = cva(
       variant: {
         default:
           'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+        destructive: redStyle,
+        negative: redStyle,
         outline:
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        positive: greenStyle,
+        success: greenStyle,
       },
       size: {
         default: 'h-9 px-4 py-2',
