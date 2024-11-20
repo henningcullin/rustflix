02f2b9f32a509cd79b7eb1f3910781f0c2bc6df0
Henning Cullin <henningcullin@gmail.com>
2024-11-20 16:18:51 +0100

    feat: edit directory action

--- src-tauri/src/directories/actions.rs
+++ src-tauri/src/directories/actions.rs
@@ -4,7 +4,7 @@ use rusqlite::params;
 use tauri::api::dialog::blocking::FileDialogBuilder;
 
 use crate::{
-    database::{create_connection, sql},
+    database::{create_connection, delete, insert, update},
     error::AppError,
     FromRow,
 };
@@ -24,6 +24,14 @@ pub fn remove_directory(id: i32) -> Result<usize, AppError> {
         &create_connection()?,
         "DELETE FROM directories WHERE id = ?1",
         params![id],
+    )
+}
+
+pub fn edit_directory(id: i32, path: &str) -> Result<usize, AppError> {
+    update(
+        &create_connection()?,
+        "UPDATE directories SET path = ?1 WHERE id = ?2",
+        params![id, path],
     )
 }
 
