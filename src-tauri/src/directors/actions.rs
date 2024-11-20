5d2436e574d70dcf615c23859007262dd4c7ae33
Henning Cullin <henningcullin@gmail.com>
2024-11-20 18:13:51 +0100

    refactor: rebuild to follow new standard

--- src-tauri/src/directors/actions.rs
+++ src-tauri/src/directors/actions.rs
@@ -1,29 +1,22 @@
 use rusqlite::params;
 
-use crate::{database::create_connection, error::AppError};
+use crate::{
+    database::{create_connection, delete, insert},
+    error::AppError,
+};
 
 pub fn delete_director(film_id: i32, person_id: i32) -> Result<usize, AppError> {
-    // Establish the database connection
-    let conn = create_connection()?;
-
-    // Prepare and execute the DELETE query
-    let rows_affected = conn.execute(
+    delete(
+        &create_connection()?,
         "DELETE FROM film_directors WHERE film_id = ?1 AND person_id = ?2",
         params![film_id, person_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
 
-pub fn create_director(film_id: i32, person_id: i32) -> Result<usize, AppError> {
-    // Establish the database connection
-    let conn = create_connection()?;
-
-    // Prepare and execute the DELETE query
-    let rows_affected = conn.execute(
+pub fn create_director(film_id: i32, person_id: i32) -> Result<i32, AppError> {
+    insert(
+        &create_connection()?,
         "INSERT INTO film_directors (film_id, person_id) VALUES (?1, ?2)",
         params![film_id, person_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
--- src-tauri/src/directors/commands.rs
+++ src-tauri/src/directors/commands.rs
@@ -21,14 +21,12 @@ pub fn delete_director(film_id: i32, person_id: i32) -> Result<(), String> {
 #[command]
 pub fn create_director(film_id: i32, person_id: i32) -> Result<(), String> {
     match actions::create_director(film_id, person_id) {
-        Ok(rows_affected) => match rows_affected {
-            0 => Err("Director was not added".into()),
-            _ => Ok(()),
-        },
+        Ok(id) => {
+            println!("Director added successfully with id: {id}");
+            Ok(())
+        }
         Err(error) => {
-            eprintln!(
-                "Error when inserting director {error}, film_id: {film_id}, person_id: {person_id}"
-            );
+            eprintln!("Error when inserting director: {error}, film_id: {film_id}, person_id: {person_id}");
             Err("Failed to add director".into())
         }
     }
