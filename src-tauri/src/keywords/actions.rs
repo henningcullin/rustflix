ca4489bc770f177c1c4bc114e4953f140e09e623
Henning Cullin <henningcullin@gmail.com>
2024-11-21 21:08:21 +0100

    refactor: use helpers

--- src-tauri/src/keywords/actions.rs
+++ src-tauri/src/keywords/actions.rs
@@ -1,6 +1,10 @@
 use rusqlite::params;
 
-use crate::{database::create_connection, error::AppError, FromRow};
+use crate::{
+    database::{create_connection, delete, insert},
+    error::AppError,
+    FromRow,
+};
 
 use super::Keyword;
 
@@ -16,46 +20,34 @@ pub fn get_all_keywords() -> Result<Vec<Keyword>, AppError> {
     Ok(keywords)
 }
 
-pub fn create_keyword(film_id: i32, keyword_id: i32) -> Result<usize, AppError> {
-    let conn = create_connection()?;
-
-    let rows_affected = conn.execute(
+pub fn create_keyword(film_id: i32, keyword_id: i32) -> Result<i32, AppError> {
+    insert(
+        &create_connection()?,
         "INSERT INTO film_keywords (film_id, keyword_id) VALUES (?1, ?2)",
         params![film_id, keyword_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
 
 pub fn delete_keyword(film_id: i32, keyword_id: i32) -> Result<usize, AppError> {
-    let conn = create_connection()?;
-
-    let rows_affected = conn.execute(
+    delete(
+        &create_connection()?,
         "DELETE FROM film_keywords WHERE film_id = ?1 AND keyword_id = ?2",
         params![film_id, keyword_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
 
-pub fn add_keyword_to_film(film_id: i32, keyword_id: &i32) -> Result<usize, AppError> {
-    let conn = create_connection()?;
-
-    let rows_affected = conn.execute(
+pub fn add_keyword_to_film(film_id: i32, keyword_id: &i32) -> Result<i32, AppError> {
+    insert(
+        &create_connection()?,
         "INSERT INTO film_keywords (film_id, keyword_id) VALUES (?1, ?2)",
         params![film_id, keyword_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
 
 pub fn remove_keyword_from_film(film_id: i32, keyword_id: &i32) -> Result<usize, AppError> {
-    let conn = create_connection()?;
-
-    let rows_affected = conn.execute(
+    delete(
+        &create_connection()?,
         "DELETE FROM film_keywords WHERE film_id = ?1 AND keyword_id = ?2",
         params![film_id, keyword_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
