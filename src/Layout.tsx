a71f8cfb222964b007f2c74d98c5cb73f6f9638a
Henning Cullin <henningcullin@gmail.com>
2024-12-28 00:17:18 +0100

    feat: sidebar trigger improvements

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,17 +1,44 @@
 import { Outlet } from 'react-router-dom';
 import AppSidebar from './AppSidebar';
-import { Toaster } from './components/ui/toaster';
-import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+import { Toaster } from '@/components/ui/toaster';
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 
-function Layout() {
+export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarTrigger />
+      <CustomSidebarTrigger />
       <Outlet />
       <Toaster />
     </SidebarProvider>
   );
 }
 
-export default Layout;
+function CustomSidebarTrigger() {
+  const { open } = useSidebar();
+
+  const sidebarTooltip = open ? 'Close sidebar' : 'Open sidebar';
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <SidebarTrigger className='mt-2 ml-2 h-8 w-8' />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{sidebarTooltip}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
