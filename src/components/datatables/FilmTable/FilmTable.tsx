08c78c311658bfbaebf22686b4a7b72f4a023036
Henning Cullin <henningcullin@gmail.com>
2025-01-04 19:32:10 +0100

    feat: release date and runtime columns

--- src/components/datatables/FilmTable/FilmTable.tsx
+++ src/components/datatables/FilmTable/FilmTable.tsx
@@ -3,6 +3,11 @@ import { ColumnDef } from '@tanstack/react-table';
 import { DataTable } from '../core';
 import { useQuery } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api/tauri';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 const columns: ColumnDef<Film>[] = [
   {
@@ -12,6 +17,43 @@ const columns: ColumnDef<Film>[] = [
   {
     accessorKey: 'title',
     header: 'Title',
+  },
+  {
+    accessorKey: 'release_date',
+    header: 'Release Date',
+  },
+  {
+    accessorKey: 'run_time',
+    header: 'Runtime',
+    cell: ({ row }) => {
+      const runTime = row.getValue('run_time');
+
+      if (typeof runTime !== 'number') return '-';
+
+      // Convert seconds to HH:MM:SS
+      const hours = Math.floor(runTime / 3600);
+      const minutes = Math.floor((runTime % 3600) / 60);
+      const seconds = runTime % 60;
+
+      // Display format: HHh MMm
+      const displayTime = `${hours}h ${minutes}m`;
+
+      // Tooltip format: HH:MM:SS
+      const tooltipTime = `${String(hours).padStart(2, '0')}:${String(
+        minutes
+      ).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+      return (
+        <Tooltip>
+          <TooltipTrigger>
+            <div>{displayTime}</div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <div>{tooltipTime}</div>
+          </TooltipContent>
+        </Tooltip>
+      );
+    },
   },
 ];
 
