45d53ce5bc6ebf9cf4f3d15d5ae6d9d81aeb7e65
Henning Cullin <henningcullin@gmail.com>
2025-01-07 20:38:05 +0100

    feat: delete film command

--- src-tauri/src/films/commands.rs
+++ src-tauri/src/films/commands.rs
@@ -1,10 +1,12 @@
-use super::Film;
+use crate::delete_command;
+
+use super::{actions, Film};
 
 use tauri::command;
 
 #[command]
 pub fn get_all_films() -> Result<Vec<Film>, String> {
-    match super::actions::get_all_films() {
+    match actions::get_all_films() {
         Ok(films) => Ok(films),
         Err(error) => {
             eprintln!("{}", error);
@@ -15,13 +17,24 @@ pub fn get_all_films() -> Result<Vec<Film>, String> {
 
 #[command]
 pub fn get_film(id: u32) -> Result<Film, String> {
-    match super::actions::get_film(id) {
+    match actions::get_film(id) {
         Ok(film) => Ok(film),
         Err(error) => {
             eprintln!("{}", error);
             Err(error.to_string()) // Return the error as a String
         }
     }
+}
+
+#[command]
+pub fn delete_film(id: i32) -> Result<(), String> {
+    delete_command!(
+        actions::delete_film(id),
+        "Film deleted successfully with id: {id}",
+        "Error when deleting film: {error}, id: {id}",
+        "Film was not deleted: id: {id}",
+        "Failed to remove film"
+    )
 }
 
 // #[command]
@@ -37,7 +50,7 @@ pub fn get_film(id: u32) -> Result<Film, String> {
 
 #[command]
 pub fn set_left_off_point(film_id: i32, played: i32) -> Result<(), String> {
-    match super::actions::set_left_off_point(film_id, played) {
+    match actions::set_left_off_point(film_id, played) {
         Ok(rows_affected) => match rows_affected {
             0 => Err("Left off point could was not stored".into()),
             _ => Ok(()),
--- src-tauri/src/films/mod.rs
+++ src-tauri/src/films/mod.rs
@@ -2,5 +2,5 @@ pub mod actions;
 pub mod commands;
 pub mod models;
 
-pub use commands::{get_all_films, get_film, set_left_off_point};
+pub use commands::{delete_film, get_all_films, get_film, set_left_off_point};
 pub use models::Film;
