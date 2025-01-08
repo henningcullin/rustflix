426de6a5a37108d7fd1573450ceeac748c408a18
Henning Cullin <henningcullin@gmail.com>
2025-01-08 19:52:35 +0100

    feat: delete film modal

--- src/films/FilmCard/tabs/InfoTab.tsx
+++ src/films/FilmCard/tabs/InfoTab.tsx
@@ -1,6 +1,15 @@
 import InfoTable, { InfoTableConfig } from '@/components/cards/InfoTable';
 import { Button } from '@/components/ui/button';
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
@@ -8,8 +17,98 @@ import {
 } from '@/components/ui/tooltip';
 import { toast } from '@/lib/hooks/use-toast';
 import { Film } from '@/lib/types';
-import { CheckIcon, Cross2Icon, ExternalLinkIcon } from '@radix-ui/react-icons';
+import {
+  CheckIcon,
+  Cross2Icon,
+  ExternalLinkIcon,
+  TrashIcon,
+} from '@radix-ui/react-icons';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { shell } from '@tauri-apps/api';
+import { invoke } from '@tauri-apps/api/tauri';
+import React from 'react';
+
+function useDeleteFilm() {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [selectedFilm, setSelectedFilm] = React.useState<Film | undefined>();
+
+  const queryClient = useQueryClient();
+
+  const deleteFilmMutation = useMutation({
+    mutationFn: async (film: Film) => {
+      await invoke('delete_film', {
+        id: film.id,
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: 'Film deleted',
+        description: `${selectedFilm?.title} was successfully removed`,
+      });
+
+      setOpen(false);
+
+      queryClient.invalidateQueries({
+        queryKey: ['film', selectedFilm?.id?.toString()],
+      });
+      queryClient.invalidateQueries({ queryKey: ['films'] });
+    },
+    onError: (error) => {
+      console.error(error);
+
+      toast({
+        variant: 'destructive',
+        title: 'Failed to delete the director',
+        description: error.message,
+      });
+    },
+  });
+
+  const handleDelete = React.useCallback(async () => {
+    if (selectedFilm && !deleteFilmMutation.isPending) {
+      deleteFilmMutation.mutate(selectedFilm);
+    }
+  }, [selectedFilm, deleteFilmMutation]);
+
+  const DeleteFilmDialog = React.useCallback(
+    () => (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Confirm deletion</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete {selectedFilm?.title}?<br />
+              This action cannot be undone.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose>
+              <Button type='button' variant='outline'>
+                <Cross2Icon className='w-5 h-5 mr-2' />
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button variant='destructive' onClick={handleDelete}>
+              <TrashIcon className='w-5 h-5 mr-2' />
+              Delete
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    ),
+    [selectedFilm, handleDelete]
+  );
+
+  function deleteFilm(film: Film) {
+    if (!deleteFilmMutation.isPending) {
+      setSelectedFilm(film);
+
+      setOpen(true);
+    }
+  }
+
+  return { DeleteFilmDialog, deleteFilm };
+}
 
 function RuntimeCell({ runTime }: { runTime?: number }) {
   if (typeof runTime !== 'number') return '-';
@@ -93,6 +192,8 @@ function CheckboxCell({ checked }: { checked: boolean }) {
 }
 
 export default function InfoTab({ film }: { film: Film }) {
+  const { DeleteFilmDialog, deleteFilm } = useDeleteFilm();
+
   const tableConfig: InfoTableConfig<Film> = {
     infoRows: [
       {
@@ -145,10 +246,13 @@ export default function InfoTab({ film }: { film: Film }) {
 
   return (
     <div className='w-full border-ws rounded-sm'>
+      <DeleteFilmDialog />
       <div className='flex place-content-center w-full mb-4'>
         <div className='inline-flex gap-3'>
           <Button>New</Button>
-          <Button variant='destructive'>Delete</Button>
+          <Button variant='destructive' onClick={() => deleteFilm(film)}>
+            Delete
+          </Button>
           <Button>Edit</Button>
         </div>
       </div>
