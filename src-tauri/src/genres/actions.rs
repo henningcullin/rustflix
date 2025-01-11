6102359e7aa4f56bd7391761c807f91e979947d1
Henning Cullin <henningcullin@gmail.com>
2025-01-11 23:36:55 +0100

    refactor: use insert and delete helpers

--- src-tauri/src/genres/actions.rs
+++ src-tauri/src/genres/actions.rs
@@ -1,6 +1,10 @@
 use rusqlite::params;
 
-use crate::{database::create_connection, error::AppError, FromRow};
+use crate::{
+    database::{create_connection, delete, insert},
+    error::AppError,
+    FromRow,
+};
 
 use super::Genre;
 
@@ -16,24 +20,18 @@ pub fn get_all_genres() -> Result<Vec<Genre>, AppError> {
     Ok(genres)
 }
 
-pub fn add_genre_to_film(film_id: i32, genre_id: &i32) -> Result<usize, AppError> {
-    let conn = create_connection()?;
-
-    let rows_affected = conn.execute(
+pub fn add_genre_to_film(film_id: i32, genre_id: &i32) -> Result<i32, AppError> {
+    insert(
+        &create_connection()?,
         "INSERT INTO film_genres (film_id, genre_id) VALUES (?1, ?2)",
         params![film_id, genre_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
 
 pub fn remove_genre_from_film(film_id: i32, genre_id: &i32) -> Result<usize, AppError> {
-    let conn = create_connection()?;
-
-    let rows_affected = conn.execute(
+    delete(
+        &create_connection()?,
         "DELETE FROM film_genres WHERE film_id = ?1 AND genre_id = ?2",
         params![film_id, genre_id],
-    )?;
-
-    Ok(rows_affected)
+    )
 }
