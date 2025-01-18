1e728b908f388944811a73c92138b98efc2ea280
Henning Cullin <henningcullin@gmail.com>
2025-01-18 11:41:04 +0100

    refactor: update commands

--- src-tauri/src/directors/commands.rs
+++ src-tauri/src/directors/commands.rs
@@ -1,33 +1,26 @@
 use tauri::command;
 
+use crate::{create_command, delete_command};
+
 use super::actions;
 
 #[command]
 pub fn add_director_to_film(film_id: i32, person_id: i32) -> Result<(), String> {
-    match actions::add_director_to_film(film_id, person_id) {
-        Ok(id) => {
-            println!("Director added successfully with id: {id}");
-            Ok(())
-        }
-        Err(error) => {
-            eprintln!("Error when inserting director: {error}, film_id: {film_id}, person_id: {person_id}");
-            Err("Failed to add director".into())
-        }
-    }
+    create_command!(
+        actions::add_director_to_film(film_id, person_id),
+        "Director ({person_id}) added successfully to film ({film_id}), film_directors id ({id})",
+        "Error when adding director ({person_id}) to film ({film_id}), error: ({error})",
+        "Failed to add director to film"
+    )
 }
 
 #[command]
 pub fn remove_director_from_film(film_id: i32, person_id: i32) -> Result<(), String> {
-    match actions::remove_director_from_film(film_id, person_id) {
-        Ok(rows_affected) => match rows_affected {
-            0 => Err("Director was not found in database".into()),
-            _ => Ok(()),
-        },
-        Err(error) => {
-            eprintln!(
-                "Error when deleting director {error}, film_id: {film_id}, person_id: {person_id}"
-            );
-            Err("Failed to delete director".into())
-        }
-    }
+    delete_command!(
+        actions::remove_director_from_film(film_id, person_id),
+        "Director ({person_id}) successfully removed from film ({film_id})",
+        "Error when removing director ({person_id}) from film ({film_id}), error: {error}",
+        "Director ({person_id}) was not removed from film ({film_id})",
+        "Failed to remove director from film"
+    )
 }
