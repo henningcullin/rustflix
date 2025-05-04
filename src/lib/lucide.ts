7720921910b6e08fd2f3f0dc181000921263c88b
henningcullin <henningcullin@gmail.com>
2025-05-04 21:37:37 +0200

    refactor: better icon handling

---
+++ src/lib/lucide.ts
@@ -0,0 +1,4 @@
+export {default as Clapperboard } from '@lucide/svelte/icons/clapperboard';
+export { default as House } from '@lucide/svelte/icons/house';
+export { default as User } from '@lucide/svelte/icons/user';
+export { default as Tv } from '@lucide/svelte/icons/tv';
