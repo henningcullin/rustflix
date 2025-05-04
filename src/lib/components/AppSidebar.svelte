63b89091ba544357982577f03ab9a75441e21049
henningcullin <henningcullin@gmail.com>
2025-05-04 09:20:23 +0200

    feat: basic items

--- src/lib/components/AppSidebar.svelte
+++ src/lib/components/AppSidebar.svelte
@@ -1,12 +1,52 @@
 <script lang="ts">
   import * as Sidebar from "$lib/components/ui/sidebar/index";
+
+  import House from "@lucide/svelte/icons/house";
+  import User from "@lucide/svelte/icons/user";
+  import type { Component } from "svelte";
+
+  type SidebarItem = {
+    title: string;
+    url: string;
+    icon: Component;
+  };
+
+  const items: SidebarItem[] = [
+    {
+      title: "Home",
+      url: "/",
+      icon: House,
+    },
+    {
+      title: "Persons",
+      url: "/persons",
+      icon: User,
+    },
+  ];
 </script>
 
 <Sidebar.Root>
   <Sidebar.Header />
   <Sidebar.Content>
-    <Sidebar.Group />
-    <Sidebar.Group />
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+        <Sidebar.Menu>
+          {#each items as item (item.title)}
+            <Sidebar.MenuItem>
+              <Sidebar.MenuButton>
+                {#snippet child({ props })}
+                  <a href={item.url} {...props}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                {/snippet}
+              </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+          {/each}
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
   </Sidebar.Content>
   <Sidebar.Footer />
 </Sidebar.Root>
