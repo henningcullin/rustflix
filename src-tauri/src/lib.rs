9bb57060710d3dc749b99b93616b0a26cd79d0fe
henningcullin <henningcullin@gmail.com>
2025-05-11 20:42:17 +0200

    feat: prepare for films

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,8 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+mod films;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
